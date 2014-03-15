4d8a4746584c2228bf6cb3f7b544c159fbfb149e
Lonique Alexander <l.w.alexander@hotmail.com>
2014-03-15 04:23:51 -0400

    completed Eulerian Tour in python

--- EulerianTour.py
+++ EulerianTour.py
@@ -1,98 +1,69 @@
 # Author: Lonique Alexander Udacity home work
-# Find Eulerian Tour
-# Write a function that takes in a graph
-# represented as a list of tuples
-# and return a list of nodes that
-# you would follow on an Eulerian Tour
-#
-# For example, if the input graph was
-# [(1, 2), (2, 3), (3, 1)]
-# A possible Eulerian tour would be [1, 2, 3, 1]
+# last updated: 03/14/2014
 
 def find_eulerian_tour(graph):
-	length=len(graph); j=0
-	origin= graph[0][0]		#The origin position
-	b=origin 				#current position
-	path=[];foundPath=False
+	length=len(graph);
+	path=[]; tour=[]; lib={}
 
-	#copy the original array to reduce complexity
-	tour= list(graph)
-	
-	#complexity of O(n^2)
-	while j<length:
+	curNode=graph[0][0]
+	while True:
+		curNode=nextMove(curNode, lib,path,graph,length)
+		if curNode==None:
 
-		origin=tour[j][0]
-
-		#now we are going to search for the 
-		# complexity of O(n)
-		for i in range(0,length):
-			if(connected(b, tour[i]) and tour[i] not in path):
-				b=tour[i]
-				path.append(tour[i])
-				del tour[i]
-				break
-
-		#i am thinking breaking this up in to different taskes
-		for t in tour:
-			print "for in tour-->", b,"=",t
-			if connected(b[1],t) and t not in path: 
-				path.append(t)
-				b=t
-				foundPath=True
+			if isTourCompleted(path,length):
+				return udacityFormat(path)
 			else:
-				foundPath=False
-				print "not connected=",b, t
-		
-		if not foundPath: 
-			backTrack(path.pop())		#path.pop() last elemented inserted 
+				curNode=path[-1][1]
+				curNode=backTrack(curNode,lib, path)
 
-		print "path=",path
-		break
-		#-------------------------------------------#
-		#this is the winning condition
-		if(j==length-1 and origin==b):
-			for p in path:
-				arrayFormPath.append(p[0])
-			arrayFormPath.append(b)
-			print "path==>",arrayFormPath
-			return True
-		#print "path update= ", path
-		#print "----------------------------------------"
-		j+=1
-	#for s in supportArray: print "supportArray",s
-	#print "path==>", path
-	return tour
+				if curNode==None: break		# No solution
+				continue
 
-def connected(x,tup):
-	if x==tup[0]: return True
+def connected(n,t): 
+	return (n==t[1] or n==t[0])
+
+def nextMove(b, lib, path, tour,length):
+	ary=[]; lib[b]=[]
+	for i in range(0,length):
+		if(connected(b, tour[i]) and isInPath(tour[i],path)):
+			y=tour[i][0] if tour[i][1]==b else tour[i][1]
+			ary.append((b,y))
+	
+	if len(ary)<=0: return None
+	
+	while len(ary)>1: lib[b].append(ary.pop())
+
+	path.append(ary.pop())
+	return path[-1][1]
+
+def isInPath(tour, path):
+	x= (tour[1],tour[0])
+	if tour not in path and x not in path:
+		return True
 	return False
 
-def nextMove(t, b, origin, path):
-	#t: tubple representing an edge
-	#b: current possition
-	#path: is a collection of all edges. the path from 1 node tot he next
+def isTourCompleted(path,length):
+	if len(path)!=length: return False		#All edges must be visited
+	if path[0][0]!=path[-1][1]:return False #check start node == last node
+	return True							#if pass these two condition then it eulerian circuit
 
-	#if curSize==
+def backTrack(b,lib, path):
+	while not len(lib[b]):
+		path.pop()
+		b=path[-1][1]
+		if len(lib)==0: return 		
 
-	return True
+	path.append(lib[b].pop())
+	return path[-1][1]
 
-def backTrack(t, b, path):
-	return True
+def udacityFormat(path):
+	newPath=[]
+	for t in path:
+		newPath.append(t[0])
+	newPath.append(path[-1][1])
+	return newPath
 
-t1= [(1, 2), (2, 3), (3, 1)]
-t2=[(0,1),(1,2),(2,0),(0,3),(3,4),(4,0)]
-#harder example then t1 and t2
-t3=[(1,0),(0,2),(2,1),(0,3),(3,4),(4,0)]
-#even harder input. see if your program know what it's doing
 t4=  [(0, 1), (1, 5), (1, 7), (4, 5),
 (4, 8), (1, 6), (3, 7), (5, 9),
 (2, 4), (0, 4), (2, 5), (3, 6), (8, 9)]
-t5=[(1, 13), (1, 6), (6, 11), (3, 13),
-(8, 13), (0, 6), (8, 9),(5, 9), (2, 6), (6, 10), (7, 9),
-(1, 12), (4, 12), (5, 14), (0, 1),  (2, 3), (4, 11), (6, 9),
-(7, 14),  (10, 13)]
-t6=[(8, 16), (8, 18), (16, 17), (18, 19),
-(3, 17), (13, 17), (5, 13),(3, 4), (0, 18), (3, 14), (11, 14),
-(1, 8), (1, 9), (4, 12), (2, 19),(1, 10), (7, 9), (13, 15),
-(6, 12), (0, 1), (2, 11), (3, 18), (5, 6), (7, 15), (8, 13), (10, 17)]
-find_eulerian_tour(t3)
+print find_eulerian_tour(t4)	
